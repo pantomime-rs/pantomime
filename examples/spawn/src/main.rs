66f90ae78b5de0d2082b47de9b137709a2f7c5cd
Jason Longshore <longshorej@gmail.com>
2019-05-20 16:42:33 -0500

    Add an example that spawns 10m actors to aid in profiling basic memory usage

---
+++ examples/spawn/src/main.rs
@@ -0,0 +1,58 @@
+extern crate pantomime;
+
+use pantomime::prelude::*;
+use std::io;
+
+const NUM_WORKERS: usize = 10_000_000;
+
+struct Worker {
+    _id: usize,
+}
+
+enum WorkerMsg {
+    Ready(ActorRef<()>),
+}
+
+impl Actor<WorkerMsg> for Worker {
+    fn receive(&mut self, msg: WorkerMsg, _: &mut ActorContext<WorkerMsg>) {
+        match msg {
+            WorkerMsg::Ready(reply_to) => {
+                reply_to.tell(());
+            }
+        }
+    }
+}
+
+struct Reaper {
+    ready: usize,
+}
+
+impl Reaper {
+    fn new() -> Self {
+        Self { ready: 0 }
+    }
+}
+
+impl Actor<()> for Reaper {
+    fn receive(&mut self, _: (), _: &mut ActorContext<()>) {
+        self.ready += 1;
+
+        if self.ready == NUM_WORKERS {
+            println!("all workers ready");
+        }
+    }
+
+    fn receive_signal(&mut self, signal: Signal, ctx: &mut ActorContext<()>) {
+        if let Signal::Started = signal {
+            for id in 1..=NUM_WORKERS {
+                let worker = ctx.spawn(Worker { _id: id });
+
+                worker.tell(WorkerMsg::Ready(ctx.actor_ref().clone()));
+            }
+        }
+    }
+}
+
+fn main() -> io::Result<()> {
+    ActorSystem::new().spawn(Reaper::new())
+}
