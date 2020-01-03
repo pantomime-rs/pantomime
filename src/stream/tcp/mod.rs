e0d44dc8a0911f79a250343bbf509dbcc20562bc
Jason Longshore <longshorej@gmail.com>
2020-01-02 18:00:06 -0600

    sketch out what tcp source could look like

--- src/stream/tcp/mod.rs
+++ src/stream/tcp/mod.rs
@@ -1,7 +1,7 @@
 use crate::actor::SubscriptionEvent;
 use crate::stream::{Action, Logic, LogicEvent, Source, Sink, StreamContext};
 use mio::{net::TcpStream, Poll, PollOpt, Ready, Token};
-use std::io::{ErrorKind as IoErrorKind, Result as IoResult, Write};
+use std::io::{ErrorKind as IoErrorKind, Read, Result as IoResult, Write};
 use std::net::SocketAddr;
 use std::sync::Arc;
 
@@ -21,6 +21,142 @@ impl Tcp {
 
     fn connect(addr: &SocketAddr) -> TcpConnection {
         unimplemented!()
+    }
+}
+
+struct TcpSource {
+    socket: IoResult<TcpStream>,
+    buffer: Vec<u8>,
+    ready: bool,
+    waiting: bool,
+    poll: Option<Arc<Poll>>,
+    token: usize,
+}
+
+impl TcpSource {
+    fn try_read(&mut self) -> Action<Vec<u8>, SubscriptionEvent> {
+        match self.socket {
+            Ok(ref mut s) if self.ready && self.waiting => {
+                match s.read(&mut self.buffer) {
+                    Ok(bytes_read) => {
+                        self.waiting = false;
+
+                        Action::Push(self.buffer[0..bytes_read].to_vec())
+                    }
+
+                    Err(ref e) if e.kind() == IoErrorKind::WouldBlock => {
+                        self.ready = false;
+
+                        Action::None
+                    }
+
+                    Err(_e) => {
+                        // @TODO
+                        unimplemented!()
+                    }
+                }
+            }
+
+            _ => Action::None,
+        }
+    }
+}
+
+impl Logic<(), Vec<u8>> for TcpSource {
+    type Ctl = SubscriptionEvent;
+
+    fn name(&self) -> &'static str {
+        "pantomime::stream::source::Udp"
+    }
+
+    fn buffer_size(&self) -> Option<usize> {
+        Some(0)
+    }
+
+    fn fusible(&self) -> bool {
+        false
+    }
+
+    fn receive(
+        &mut self,
+        msg: LogicEvent<(), Self::Ctl>,
+        ctx: &mut StreamContext<(), Vec<u8>, Self::Ctl>,
+    ) -> Action<Vec<u8>, Self::Ctl> {
+        match msg {
+            LogicEvent::Pulled => {
+                //println!("got pulled");
+                self.waiting = true;
+
+                self.try_read()
+            }
+
+            LogicEvent::Forwarded(SubscriptionEvent::MioEvent(event)) => {
+                //println!("source got a mio event");
+                if event.readiness().is_readable() {
+                    self.ready = true;
+
+                    self.try_read()
+                } else {
+                    Action::None
+                }
+            }
+
+            LogicEvent::Forwarded(SubscriptionEvent::Ready(poll, token)) => {
+                //println!("source got a ready");
+                match self.socket {
+                    Ok(ref socket) => {
+                        // @TODO expect doesn't seem appropriate
+                        poll.register(socket, Token(token), Ready::readable(), PollOpt::edge())
+                            .expect("pantomime bug: failed to register socket");
+
+                        self.poll = Some(poll);
+                        self.token = token;
+
+                        self.try_read()
+                    }
+
+                    Err(ref _e) => {
+                        // shouldn't happen
+
+                        ctx.unsubscribe(token);
+
+                        Action::None
+                    }
+                }
+            }
+
+            LogicEvent::Started => {
+                let actor_ref = ctx.stage_ref().actor_ref.clone();
+
+                ctx.subscribe(actor_ref);
+
+                Action::None
+            }
+
+            LogicEvent::Stopped => Action::None,
+
+            LogicEvent::Cancelled => {
+                //println!(" SOURCE CANCELLED!");
+                if let Some(poll) = self.poll.take() {
+                    if let Ok(ref socket) = self.socket {
+                        // @TODO expect doesn't seem appropriate
+
+                        poll.deregister(socket)
+                            .expect("pantomime bug: failed to deregister socket");
+                    }
+                }
+
+                if self.token > 0 {
+                    ctx.unsubscribe(self.token);
+
+                    self.token = 0;
+                }
+
+                Action::Stop(None)
+            }
+
+            LogicEvent::Pushed(()) => Action::None,
+        }
     }
 }
 
@@ -69,6 +205,8 @@ impl TcpSink {
                                 Action::Pull
                             }
                         } else {
+                            // @TODO should we call try_write again??
+                            //       i.e. call it until exhausted or rxd WouldBlock
                             Action::None
                         }
                     }
